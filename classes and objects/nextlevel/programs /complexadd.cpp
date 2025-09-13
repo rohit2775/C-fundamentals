9fb88f9b6eb14d2ca1a391e42dca11cbf9dc8ded
rohit2775 <rg7558674@gmail.com>
2025-09-13 10:43:59 +0530

    reaching to make programs

---
+++ classes and objects/nextlevel/programs /complexadd.cpp
@@ -0,0 +1,40 @@
+
+ #include<iostream>
+ using namespace std ;
+
+ class complexno {
+    private:
+    int a;
+    int b;
+    public:
+    void setdata(int x , int y ){
+    a=x;
+    b=y;
+    }
+    void showdata(){
+        cout<<"a="<<a<<"b="<<b<<endl;
+
+    }
+
+    complexno add (complexno c ){
+       complexno temp ;
+        temp.a=a+c.a;
+        temp.b=b+c.b;
+
+        return temp;
+
+
+    }
+
+ };
+
+ int main (){
+
+     complexno c1,c2,c3;
+     c1.setdata(3,5);
+     c2.setdata(6,7);
+     c3=c1.add(c2);
+
+
+    return 0;
+ }
